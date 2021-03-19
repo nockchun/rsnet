8886c2d05837ae724265299e89ab41d0ba9d11c3
nockchun <nockchun@gmail.com>
2021-03-19 19:38:24 +0900

    docker file upload

---
+++ lecture_source/python/dockerize/mongo_flask/app/app.py
@@ -0,0 +1,40 @@
+import os, json
+from flask import Flask
+from pymongo import MongoClient
+
+app = Flask(__name__)
+env_username = os.getenv("MONGO_USERNAME", "admin")
+env_password = os.getenv("MONGO_PASSWORD", "password")
+env_server = os.getenv("MONGO_SERVER", "localhost")
+
+connstr = f"mongodb://{env_username}:{env_password}@{env_server}:27017"
+client = MongoClient(connstr)
+db = client.demodb
+collect = db.data1
+
+@app.route("/")
+def default():
+    return "I'm Flask...!"
+
+@app.route("/init")
+def initDB():
+    doc = [
+        {"name": "홍길동","age": 27},
+        {"name": "이순신","age": 45},
+        {"name": "김유신","age": 27},
+    ]
+    collect.insert_many(doc)
+    return "document created"
+
+@app.route("/list")
+def listingDB():
+    collections = db.list_collection_names()
+    collections_str = json.dumps(collections)
+
+    documents = collect.find({"age": 27})
+    documents_str = json.dumps([doc["name"] for doc in documents], ensure_ascii = False)
+    
+    return f"collections: {collections_str}<br>documents: {documents_str}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
